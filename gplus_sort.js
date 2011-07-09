1431504079aff97e037f172de3cc50f611fdf25d
uxp <uxp@hplogsdon.com>
2011-07-09 12:51:33 -0600

    Build some an array of data, which are arrays of the timestamp, the div that holds the timestamp, and the div that holds each status update

--- gplus_sort.js
+++ gplus_sort.js
@@ -11,5 +11,25 @@ if (document.readyState == "complete") {
 		}
 		return true;
 	}
+	function getStreamPosts() {
+		var $streamDiv = document.getElementsByClassName('a-b-f-i-oa');
+		if (($streamDiv.length == 1) && ($streamDiv[0].children.length > 0)) {
+			var data = [];
+			for (var i=0; i < $streamDiv[0].children.length; i++) {
+				data[i] = [];
+				data[i][0] = Date.parse($streamDiv[0].children[i].getElementsByClassName('a-Ja-h a-b-h-Jb a-f-i-Ad')[0].title);
+				data[i][1] = $streamDiv[0].children[i].getElementsByClassName('a-Ja-h a-b-h-Jb a-f-i-Ad')[0];
+				data[i][2] = $streamDiv[0].children[i];
+			}
+			// sort stream
+
+			// rebuild stream
+
+			// redraw list
+		}
+	}
+
+	getStreamPosts();
+
 // javascript:(function(){document.body.appendChild(document.createElement('script')).src='http://localhost/~uxp/gplus_sort.js';})();
 }
