17dfa153f4711a99ec880a99f6aced0c1fea81a7
uxp <uxp@hplogsdon.com>
2011-07-09 12:50:06 -0600

    We need a prototype compare function for our arrays... I think

--- gplus_sort.js
+++ gplus_sort.js
@@ -1,4 +1,15 @@
 
 if (document.readyState == "complete") {
+
+	Array.prototype.compare = function(testArr) {
+		if (this.length != testArr.length) return false;
+		for (var i = 0; i < testArr.length; i++) {
+			if (this[i].compare) {
+				if (!this[i].compare(testArr[i])) return false;
+			}
+			if (this[i] !== testArr[i]) return false;
+		}
+		return true;
+	}
 // javascript:(function(){document.body.appendChild(document.createElement('script')).src='http://localhost/~uxp/gplus_sort.js';})();
 }
