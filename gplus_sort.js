c30822ebd160d9b9e7a39129b2f597e61c601a17
uxp <uxp@hplogsdon.com>
2011-07-09 20:33:13 -0600

    Looks like I'm no longer using this Array.compare. Lets get rid of it.

--- gplus_sort.js
+++ gplus_sort.js
@@ -1,17 +1,6 @@
 
 if (document.readyState == "complete") {
 
-	Array.prototype.compare = function(testArr) {
-		if (this.length != testArr.length) return false;
-		for (var i = 0; i < testArr.length; i++) {
-			if (this[i].compare) {
-				if (!this[i].compare(testArr[i])) return false;
-			}
-			if (this[i] !== testArr[i]) return false;
-		}
-		return true;
-	}
-
 	// Iterative Bubble Sort... recursion probably isn't the best for JS
 	function sortStreamArray(data) {
 		var N = data.length;
